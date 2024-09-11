ff5202f719e92bb4ecb770738b11a7ee97cbf370
Hiagoca <hiagoca.senac@gmail.com>
2024-09-11 15:32:13 -0300

    2024.09.11
    Alterações finais feitas na aula do dia 11.09.2024.

--- js/template.js
+++ js/template.js
@@ -1,24 +1,59 @@
 function runApp() {
-    $('#wrap>nav').hide();
-    $('#fumaca').hide();
-    $('#toggleMenu').click(controlaMenu)
+
+    /* Oculta o menu na inicialização */
+    resizeMenu();
+
+    /* Quando a janela for redimensionada chama resizeMenu() */
+    $(window).resize(resizeMenu);
+
+    /* Quando do botão do menu for clicado chama a função toggleMenu() */
+    $('#toggleMenu').click(toggleMenu);
 }
 
-function controlaMenu() {
+function resizeMenu() {
+
+    /*
+    Se a largura da tela é maior que 639px, exibe 0 nemu (showMenu)
+    e oculta o smoke (noSmoke = true).
+    Se não (else), sempre oculta o menu (hideMenu)
+
+    Resolve o dilerma da sanfona.
+    */
+    if (window.innerWidth >= 600) {
+        showMenu(true);
+    } else {
+        hideMenu();
+    }
+}
+
+function toggleMenu() {
 
     if ($('#wrap>nav').is(':visible')) {
-        $('#wrap>nav').hide('fast');
-        $('#fumaca').hide('fast');
-        $('body').css('overflow', 'auto');
+        hideMenu();
     } else {
-        $('#wrap>nav').show('fast');
-        $('#fumaca').show('fast');
-        $('body').css('overflow', 'hidden');
+        showMenu();
     }
-
     return false;
 }
 
+/* Exibe o menu */
+function showMenu(noSmoke) {
+    $('#wrap>nav').show('fast');
 
+    if (noSmoke) {
+        $('#menuSmoke').hide('fast');
+        $('body').css('overflow', 'auto');
+    } else {
+        $('#menuSmoke').show('fast');
+        $('body').css('overflow', 'hidden');
+    }    
+}
+
+/* oculta o menu */
+function hideMenu() {
+    $('#wrap>nav').hide('fast');
+    $('#menuSmoke').hide('fast');
+    $('body').css('overflow', 'auto');
+}
 
 $(document).ready(runApp);
